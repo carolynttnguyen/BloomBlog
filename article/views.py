585325dad8a6525a09546b72b3f12c8d6cc0125f
carongyn <carolynttnguyen@gmail.com>
2021-04-29 01:07:21 -0700

    added delete funtion

--- article/views.py
+++ article/views.py
@@ -9,13 +9,25 @@ def article_create(request):
     # if user submits data
     if request.method == "POST":
         # assign submitted data to the form instance      
-        article_post_from = ArticlePostForm(data=request.POST)
+        article_post_form = ArticlePostForm(data=request.POST)
         # see if data submitted meets requirement of model
-        if article_post_from.is_valid():
+        if article_post_form.is_valid():
             # save data, but not to db for now
-            new_article = article_post_from.save(commit=False)
+            new_article = article_post_form.save(commit=False)
             # specify user id in db as author
-            
+            new_article.author=User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+            # if data suer enter is invalid
+        else:
+            return HttpResponse("The content of the form is wrong, please fill in again.")
+        # if the user request data
+    else:
+        article_post_form = ArticlePostForm()
+        context= {
+            'article_post_form' : article_post_form,
+        }
+        return render(request, 'article/create.html', context)
 
 def article_list(request):
     # combine all blog post
@@ -26,7 +38,7 @@ def article_list(request):
     }
     return render(request, 'article/list.html', context)
 
-def article_details(request, id):
+def article_details(request,id):
     article = ArticlePost.objects.get(id=id)
     article.body = markdown.markdown(article.body,
                                      extensions=[
@@ -37,3 +49,9 @@ def article_details(request, id):
         'article': article,
     }
     return render(request, 'article/detail.html', context)
+
+def article_delete(request, id):
+    # capture article id to be deleted
+    article = ArticlePost.objects(id=id)
+    article.delete()
+    return redirect("article:article_list")
