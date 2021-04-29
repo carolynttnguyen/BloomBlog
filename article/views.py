fcf24f7525bc5601483122f2c805651aba00d8d2
carongyn <carolynttnguyen@gmail.com>
2021-04-28 23:02:55 -0700

    article details added, and writing with markdown

--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+import markdown
+
 
 def article_list(request):
     # combine all blog post
@@ -11,9 +13,15 @@ def article_list(request):
     }
     return render(request, 'article/list.html', context)
 
-def article_detail(request, id):
+
+def article_details(request, id):
     article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         'markdown.extensions.extra',
+                                         'markdown.extensions.codehilite',
+                                     ])
     context = {
         'article': article,
     }
-    return render (request, 'article/detail.html', context)
+    return render(request, 'article/detail.html', context)
