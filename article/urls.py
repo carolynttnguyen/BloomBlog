2ec9028e87b98203f40f7606203ef9bb8a8fa6ac
carongyn <carolynttnguyen@gmail.com>
2021-05-11 00:32:51 -0700

    update function added, basic CRUD cmmds have been added

--- article/urls.py
+++ article/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('article-list', views.article_list, name ='article_list'),
     path('article-detail/<int:id>/', views.article_details, name='article_detail'),
     path('article-create/', views.article_create, name='article_create'),
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
     path('article-safe-delete/<int:id>/', views.article_safe_delete, name='article_safe_delete'),
     path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
 ]
--- article/views.py
+++ article/views.py
@@ -5,6 +5,7 @@ from .forms import ArticlePostForm
 from django.contrib.auth.models import User
 import markdown
 
+# CREATE
 def article_create(request):
     # if user submits data
     if request.method == "POST":
@@ -29,6 +30,7 @@ def article_create(request):
         }
         return render(request, 'article/create.html', context)
 
+# Show All article_safe_delete
 def article_list(request):
     # combine all blog post
     articles = ArticlePost.objects.all()
@@ -38,6 +40,7 @@ def article_list(request):
     }
     return render(request, 'article/list.html', context)
 
+# READ
 def article_details(request,id):
     article = ArticlePost.objects.get(id=id)
     article.body = markdown.markdown(article.body,
@@ -50,12 +53,41 @@ def article_details(request,id):
     }
     return render(request, 'article/detail.html', context)
 
+# UPDATE
+def article_update(request, id):
+    # retrieve specific article object that is going to be modified
+    article = ArticlePost.objects.get(id=id)
+    # determine whether user submits the form data as POST
+    if request.method == 'POST':
+        # assign data to the form instance
+        article_post_form = ArticlePostForm(data = request.POST)
+        # does data submitted meet the requirements for model
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            return redirect('article:article_detail', id=id )
+        else: 
+            return HttpResponse('The content of the form is incorrect, please fill again.')
+    # if User sends GET request
+    else:
+        # create a form class instance
+        article_post_form = ArticlePostForm()
+        # assign a context, pass the article object in to extract old content
+        context = {
+            'article': article,
+            'article_post_form' : article_post_form
+        }
+        return render(request, 'article/update.html', context)
+
 def article_delete(request, id):
     # capture article id to be deleted
     article = ArticlePost.objects.get(id = id)
     article.delete()
     return redirect("article:article_list")
 
+# DELETE
+# Safely Delete the article, passing ID to function
 def article_safe_delete(request, id):
     if request.method =='POST':
         article = ArticlePost.objects.get(id=id)
@@ -63,3 +95,4 @@ def article_safe_delete(request, id):
         return redirect('article:article_list')
     else:
         return HttpResponse("Only allow POSt requests!")
+
