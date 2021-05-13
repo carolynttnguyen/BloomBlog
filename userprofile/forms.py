1fd053dc6d8dc6613a962aa4f2f745ad7197a214
carongyn <carolynttnguyen@gmail.com>
2021-05-13 00:26:19 -0700

    user registration set up

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -4,3 +4,21 @@ from django.contrib.auth.models import User
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+class UserRegisterForm(forms.ModelForm):
+    # copy user's pw'
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+        # check if passwords are consistent 
+        def clean_password2(self):
+            data = self.cleaned_data
+            if data.get('password') == data.get('password2'):
+                return data.get('password')
+            else:
+                raise forms.ValidationError('Password input is not the same, please try again.')
+
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 # Create your views here.
+
+def user_register(request):
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            # set Pw
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            # login immediately after saving the data ad return to the blog list 
+            login(request, new_user)
+            return redirect('article:article_list')
+        else:
+            return HttpResponse('Registration inputs are invalid. Please Re-enter')
+    elif request.method =='GET':
+        user_register_form = UserRegisterForm()
+        context = {
+            'form' : user_register_form
+            }
+        return render(request, 'userprofile/register.html', context)
+    else:
+        return HttpResponse('Please use Get or Post request for Data.')
 
 def user_login(request):
     if request.method =='POST':
